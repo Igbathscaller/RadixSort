d0bfab6a0d8b9c3bbd9e410a0a963701f8c032e3
Igbathscaller <Igbathscaller@gmail.com>
2021-01-13 17:06:54 -0500

    SortableLikedList functions

--- Radix.java
+++ Radix.java
@@ -3,11 +3,11 @@ public class Radix {
     //get nth digit of an int, where 0 is the ones column, 1 is the tens column etc.
 
     public static int nth(int n, int col){
-        int divisor = 10;
+        int divisor = 1;
         for (int i = 0; i < col; i++){
             divisor *= 10;
         } 
-        return Math.abs(n) % divisor;
+        return (Math.abs(n) / divisor) % 10;
     }
 
     //return the number of digits in n.
@@ -15,11 +15,30 @@ public class Radix {
     public static int length(int n){
         return (int)Math.floor(Math.log(n))+1;
     }
-    
-    //Merge all of the linked lists in the bucket array into your original linked list. The original may have elements, and those should be kept.
-
-    public static void merge(MyLinkedList original,MyLinkedList[]buckets){
-
+    /*
+    Merge all of the linked lists in the bucket array into your original linked list. The original may have elements, and those should be kept.
+    This is O(buckets.length) which should be 10 when we use this later.
+    The bucket[0] list will be merged first, then the bucket[1] etc.
+    */    
+    public static void merge(SortableLinkedList original, SortableLinkedList[]buckets){
+        for( int i = 0; i < buckets.length; i++){
+            original.extend(buckets[i]);
+        }
     }
 
+    //Write a method that sorts non-negative integer values
+    public static void radixSortSimple(SortableLinkedList data){
+        int maxlength = 1;
+        int current;
+        SortableLinkedList[] buckets = new SortableLinkedList[10];
+        for(int i = 0; i<maxlength;i++){
+            while(data.size() > 0){
+                current = data.remove(0);
+                buckets[current%10].add(current);
+                if (current > maxlength){
+                    maxlength=length(current);
+                }
+            }
+        }
+    }
 }
